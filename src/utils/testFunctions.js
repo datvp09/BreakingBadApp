507eb88164a78fcb7ed055df571aa311d836b6b8
Dat Tran <dat.tran@syntrinos.vn>
2021-03-31 14:03:35 +0700

    update function

---
+++ src/utils/testFunctions.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {render as renderComponent} from 'react-native-testing-library';
+import {Provider} from 'react-redux';
+import createMockStore from 'redux-mock-store';
+
+const Stack = createStackNavigator();
+
+const MockRootNavigator = ({component}) => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={'Public'}>{props => component}</Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export const renderForTest = (Component, {initialState} = defaultOptions) => {
+  const mockStore = createMockStore(initialState);
+  return renderComponent(
+    <Provider store={mockStore}>
+      <MockRootNavigator component={Component} />
+    </Provider>,
+  );
+};
